ce60ad8bea05fc36c7657087527d33cd8506109d
Abigail Mariano <abigail@pixsell.com.sg>
2021-02-12 23:10:23 +0800

    Moved Utility files in one folder and created BaseView and new custom class for UIView.

--- JobOrderApp/Base/BaseView.swift
+++ JobOrderApp/Base/BaseView.swift
@@ -5,4 +5,26 @@
 //  Created by Abigail Mariano on 2/11/21.
 //
 
-import Foundation
+import UIKit
+
+class BaseView: UIView {
+    public var identifier: String! {
+        return "\(self.typeName(self))"
+    }
+    
+    func initView() { }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initView()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initView()
+    }
+    
+    private func typeName(_ some: Any) -> String {
+        return (some is Any.Type) ? "\(some)" : "\(type(of: some))"
+    }
+}
---
+++ JobOrderApp/SupportingFiles/Utilities/Views/ViewWithBottomBorder.swift
@@ -0,0 +1,14 @@
+//
+//  ViewWithBottomBorder.swift
+//  JobOrderApp
+//
+//  Created by Abigail Mariano on 2/12/21.
+//
+
+import UIKit
+
+class ViewWithBottomBorder: BaseView {
+    override func initView() {
+        self.add(Border: .bottom)
+    }
+}
