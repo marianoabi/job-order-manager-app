b334e876a83d44d0b8e371a609f2e45c34d82b24
Abigail Mariano <abigail@pixsell.com.sg>
2021-02-13 21:11:39 +0800

    Persist UserInterfaceStyle to Light Mode

--- JobOrderApp/App/SceneDelegate.swift
+++ JobOrderApp/App/SceneDelegate.swift
@@ -15,6 +15,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
         self.setupObservers()
+        
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .light
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
