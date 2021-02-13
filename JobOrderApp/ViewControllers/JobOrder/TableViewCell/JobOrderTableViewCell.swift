00f16bf3d7375be85bb11ea79f4a32b593c49733
Abigail Mariano <abigail@pixsell.com.sg>
2021-02-13 22:20:06 +0800

    Created a function to convert hex to UIColor

--- JobOrderApp/ViewControllers/JobOrder/TableViewCell/JobOrderTableViewCell.swift
+++ JobOrderApp/ViewControllers/JobOrder/TableViewCell/JobOrderTableViewCell.swift
@@ -61,6 +61,31 @@ class JobOrderTableViewCell: UITableViewCell {
     
     private func setupJobStatus(_ status: JobStatus) {
         self.statusLabel.text = status.name
-//        self.statusLabel.textColor = UIColor.green
+
+        if let hex = status.colorHex {
+            self.statusLabel.textColor = hexStringToUIColor(hex: hex)
+        }
+    }
+    
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 }
